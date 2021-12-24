e7d3a569c60e5631847cf13d45aaaa7d1f05cf44
Shreyayerraballa <shreyayerraballa@gmail.com>
2021-12-24 18:31:17 +0530

    Day10

---
+++ GetBytes.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class GetBytes{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        byte[] b=str.getBytes();
+        for(int i =0;i<b.length;i++){
+            System.out.println(b[i]);
+        }
+        String str1=new String(b);
+        System.out.println(str1);
+        sc.close();
+    }
+} 
+//output:
+//-------
+//shreya
+//115
+//104
+//114
+//101
+//121
+//97
+//shreya
